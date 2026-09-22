01dbfcd7da531cdb411881722e30de522a035d6d
leekyoungjae87-creator <leekyoungjae87@gmail.com>
2026-09-22 22:41:32 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'kd-allplay-v76-42';
+const CACHE_NAME = 'kd-allplay-v76-44';
 const CORE = [
   './',
   './index.html',
   './styles.css',
-  './config.js?v=76.43',
-  './app.js?v=76.43',
-  './1-1.jpg','./1-2.jpg','./1-3.jpg','./1-4.jpg','./1-5.jpg','./1-6.jpg','./1-7.jpg','./2-8.jpg','./3-2.jpg'
+  './config.js?v=76.44',
+  './app.js?v=76.44',
+  './1-1.jpg','./1-2.jpg','./1-3.jpg','./1-4.jpg','./1-5.jpg','./1-6.jpg','./1-7.jpg','./2-2.jpg','./2-4.jpg','./2-7.jpg','./2-8.jpg','./3-1.jpg','./3-2.jpg','./3-4.jpg'
 ];
 
 self.addEventListener('install', event => {
