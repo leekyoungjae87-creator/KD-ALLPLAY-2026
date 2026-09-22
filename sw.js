026f4342b79d8ef0f15312f8cdf7c34d26710270
leekyoungjae87-creator <leekyoungjae87@gmail.com>
2026-09-22 23:55:31 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ const CORE = [
   './index.html',
   './styles.css',
   './config.js?v=76.47',
-  './app.js?v=76.55',
+  './app.js?v=76.56',
   './prelim-0930.png','./patbingsu-booth.png','./1-1.jpg','./1-2.jpg','./1-3.jpg','./1-4.jpg','./1-5.jpg','./1-6.jpg','./1-7.jpg','./2-2.jpg','./2-4.jpg','./2-7.jpg','./2-8.jpg','./3-1.jpg','./3-2.jpg','./3-4.jpg'
 ];
 
