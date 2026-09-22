8c9205ea5932f19fda10498ee0657d6c44adc94c
leekyoungjae87-creator <leekyoungjae87@gmail.com>
2026-09-22 23:50:18 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'kd-allplay-v76-53';
+const CACHE_NAME = 'kd-allplay-v76-55';
 const CORE = [
   './',
   './index.html',
   './styles.css',
   './config.js?v=76.47',
-  './app.js?v=76.53',
+  './app.js?v=76.55',
   './prelim-0930.png','./patbingsu-booth.png','./1-1.jpg','./1-2.jpg','./1-3.jpg','./1-4.jpg','./1-5.jpg','./1-6.jpg','./1-7.jpg','./2-2.jpg','./2-4.jpg','./2-7.jpg','./2-8.jpg','./3-1.jpg','./3-2.jpg','./3-4.jpg'
 ];
 
