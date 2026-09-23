8c96b00f8b0b7cbdec02ba7a0d9b197e89b1c2ff
leekyoungjae87-creator <leekyoungjae87@gmail.com>
2026-09-23 15:23:56 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -346,7 +346,7 @@ findStudent.onclick=()=>{
 const BUILTIN_FLAG_IMAGES={
   '1-1':'1-1.jpg','1-2':'1-2.jpg','1-3':'1-3.jpg','1-4':'1-4.jpg',
   '1-5':'1-5.jpg','1-6':'1-6.jpg','1-7':'1-7.jpg',
-  '2-1':'2-1.jpg','2-2':'2-2.jpg','2-3':'2-3.jpg','2-4':'2-4.jpg','2-6':'2-6.jpg','2-7':'2-7.jpg','2-8':'2-8.jpg',
+  '2-1':'2-1.jpg','2-2':'2-2.jpg','2-3':'2-3.jpg','2-4':'2-4.jpg','2-5':'2-5.jpg','2-6':'2-6.jpg','2-7':'2-7.jpg','2-8':'2-8.jpg',
   '3-1':'3-1.jpg','3-2':'3-2.jpg','3-3':'3-3.jpg','3-4':'3-4.jpg','3-5':'3-5.jpg','3-6':'3-6.jpg','3-7':'3-7.jpg'
 };
 function flagImageFor(key,store){return (store&&store[key])||BUILTIN_FLAG_IMAGES[key]||'';}
